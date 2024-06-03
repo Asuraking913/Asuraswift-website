5f33caa08779f705fa2af8057929f7be2a116ca6
Israel Shedrack <israelshedrack913@gmail.com>
2024-06-02 21:55:55 -0400

    Creating frontend interface for asuraswift website

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div className='flex justify-between items-center p-[1em] px-[2.5em] border-b-2 border-[#00000056]'>
+        <a href="" className='font-[lit] text-2xl font-extrabold text-yellow-300'>Asura<span className='text-blue-600'>Swift</span></a>
+        <nav className=''>
+            <ul className='flex items-center gap-[4em]'>
+                {/* <li><a href="">Home</a></li> */}
+                <li><a className='text-[1.2rem] text-blue-600 font-semibold font-[roboto]' href="">About</a></li>
+                <li><a className='text-[1.2rem] text-blue-600 font-semibold font-[roboto]' href="">Developer?</a></li>
+                <li><a className='text-[1.2rem] text-blue-600 font-semibold font-[roboto]' href="">Feedback</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Head
